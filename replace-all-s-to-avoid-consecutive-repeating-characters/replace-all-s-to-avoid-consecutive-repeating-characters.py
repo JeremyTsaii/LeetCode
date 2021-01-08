dd6dd647b54a998b9037072e50ca13f5bf1cf4f6
Jeremy Tsai <jetsai@hmc.edu>
2021-01-08 00:40:39 -0800

    Time: 48 ms (5.28%), Space: 14.3 MB (45.79%) - LeetHub

--- replace-all-s-to-avoid-consecutive-repeating-characters/replace-all-s-to-avoid-consecutive-repeating-characters.py
+++ replace-all-s-to-avoid-consecutive-repeating-characters/replace-all-s-to-avoid-consecutive-repeating-characters.py
@@ -1,27 +1,17 @@
 class Solution:
     def modifyString(self, s: str) -> str:
-        output = ''
+        output = list(s)
         
-        for i in range(len(s)):
-            if s[i] == '?':
-                left = i - 1
-                right = i + 1
-​
-                if left < 0:
-                    val = ord(s[right]) + 1 if right < len(s) else ord('a')
-                    if not (ord('a') <= val <= ord('z')):
-                        val = ord('a')
-                    output += chr(val)
-                elif right >= len(s) or s[right] == '?':
-                    val = ord(output[left]) + 1
-                    if not (ord('a') <= val <= ord('z')):
-                        val = ord('a')
-                    output += chr(val)
-                else:
-                    val = (ord(output[left]) + ord(s[right]))//2
-                    if val == ord(output[left]) or val == ord(s[right]):
-                        val = min(ord(output[left]), ord(s[right])) -1
-                        if val < ord('a'):
-                            val = ord('z')
-                    output += chr(val)
-            else:
+        for i in range(len(output)):
+            if output[i] == "?":
+                left = output[i-1] if i > 0 else 'a'
+                right = output[i+1] if i < len(s)-1 and output[i+1] != '?' else 'a'
+                if left != 'a' and right != 'a':
+                    output[i] = 'a'
+                if left != 'b' and right != 'b':
+                    output[i] = 'b'
+                if left != 'c' and right != 'c':
+                    output[i] = 'c'
+                    
+        return "".join(output)
+                
