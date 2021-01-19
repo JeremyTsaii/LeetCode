c0ba1a2f60d51359fa08b493377138cc89115ee1
Jeremy Tsai <jetsai@hmc.edu>
2021-01-19 12:04:51 -0800

    Time: 5 ms (54.91%), Space: 35.7 MB (62.41%) - LeetHub

---
+++ consecutive-numbers-sum/consecutive-numbers-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int consecutiveNumbersSum(int N) {
+        int count = 0;
+        
+        for (int i = 1; i * (i - 1) / 2 < N; i++) {
+            if ((N - i * (i - 1) / 2 ) % i == 0) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
