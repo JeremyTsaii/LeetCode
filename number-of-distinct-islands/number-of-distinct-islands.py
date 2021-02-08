70b67d75ce26df96e5f23308bc2922bec7a3eb6a
Jeremy Tsai <jetsai@hmc.edu>
2021-02-08 10:08:50 -0800

    Time: 208 ms (86.81%), Space: 14.6 MB (91.92%) - LeetHub

--- number-of-distinct-islands/number-of-distinct-islands.py
+++ number-of-distinct-islands/number-of-distinct-islands.py
@@ -1,24 +1,27 @@
 class Solution:
-    def numDistinctIslands(self, grid: List[List[int]]) -> int:
-        dic = set()
-        neighbors = [(1,0,'a'), (0,1,'b'), (-1,0,'b'), (0,-1,'d')]
-        stack = []
-        
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    grid[i][j] = 0
-                    stack.append((i,j))
-                    uniqStr = ""
-                    while stack:
-                        x, y = stack.pop()
-                        for dx, dy, char in neighbors:
-                            xx = x + dx
-                            yy = y + dy
-                            if xx >= 0 and xx < len(grid) and yy >= 0 and yy < len(grid[0]) and grid[xx][yy] == 1:
-                                uniqStr += char + str(len(stack))
-                                stack.append((xx,yy))
-                                grid[xx][yy] = 0
-                    if uniqStr not in dic:
-                        dic.add(uniqStr)
-        return len(dic)
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+        dic = set()
+        seen = set()
+        dirs = [(0,1,'a'),(1,0,'b'),(-1,0,'c'),(0,-1,'d')]
+        stack = []
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    stack.append((i,j))
+                    grid[i][j] = 0
+                    cur = []
+                    while stack:
+                        x, y = stack.pop()
+                        for dx, dy, char in dirs:
+                            xx = x + dx
+                            yy = y + dy
+                            
+                            if xx >= 0 and xx < len(grid) and yy >= 0 and yy < len(grid[0]) and grid[xx][yy] == 1:
+                                grid[xx][yy] = 0
+                                stack.append((xx,yy))
+                                cur.append(char + str(len(stack)))
+
+                    dic.add("".join(cur))
+                
+        return len(dic)
