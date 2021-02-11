ba1ca28c9f9fe9737cac2de64d89d621d8620bea
Jeremy Tsai <jetsai@hmc.edu>
2021-02-11 14:20:19 -0800

    Time: 408 ms (62.54%), Space: 14.2 MB (99.65%) - LeetHub

---
+++ task-scheduler/task-scheduler.py
@@ -0,0 +1,21 @@
+from collections import deque
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        if not n:
+            return len(tasks)
+        
+        freqs = [0] * 26
+        
+        for task in tasks:
+            freqs[ord(task) - ord('A')] += 1
+        
+        freqs.sort()
+        
+        maximum = freqs.pop()
+        idle = (maximum-1) * n
+        
+        while freqs and idle > 0:
+            idle -= min(freqs.pop(), maximum - 1)
+        idle = max(0, idle)
+        
+        return idle + len(tasks)
