73c5d83e89d425f0962a055b00d331695ed335d8
Jeremy Tsai <jetsai@hmc.edu>
2021-02-14 11:46:00 -0800

    Time: 40 ms (47.98%), Space: 14.4 MB (52.28%) - LeetHub

--- group-shifted-strings/group-shifted-strings.py
+++ group-shifted-strings/group-shifted-strings.py
@@ -3,7 +3,7 @@ class Solution:
         dic = {}
         
         for string in strings:
-            shifts = tuple(ord(string[i]) - ord(string[i+1]) + 26 if ord(string[i]) - ord(string[i+1]) < 0 else ord(string[i]) - ord(string[i+1]) for i in range(len(string)-1))
+            shifts = tuple((ord(string[i]) - ord(string[i+1])) % 26 for i in range(len(string)-1))
             dic[shifts] = dic.get(shifts, []) + [string]
             
         return dic.values()
