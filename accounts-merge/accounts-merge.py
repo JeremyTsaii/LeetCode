72ee7d7e409850de391205b2235551c8a42807d7
Jeremy Tsai <jetsai@hmc.edu>
2021-02-19 09:04:35 -0800

    Time: 240 ms (47.03%), Space: 18.4 MB (67.57%) - LeetHub

--- accounts-merge/accounts-merge.py
+++ accounts-merge/accounts-merge.py
@@ -1,39 +1,38 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        output = []
-        
         graph = {}
         
         for account in accounts:
             name = account[0]
-            for mail in account[1:]:
-                graph[mail] = [name, set()]
-        
-        for account in accounts:
-            for i in range(1, len(account)):
-                for j in range(1, len(account)):
-                    if i != j:
-                        graph[account[i]][1].add(account[j])
-                        graph[account[j]][1].add(account[i])
-        
+            emails = account[1:]
+            for mail in emails:
+                if mail not in graph:
+                    graph[mail] = [name, []]
+                for mail2 in emails:
+                    if mail != mail2:
+                        graph[mail][1].append(mail2)
+            
+        output = []
         seen = set()
+
         for mail in graph:
             if mail not in seen:
+                name, _ = graph[mail]
                 stack = [mail]
                 seen.add(mail)
 
-                person = []
+                cur = []
 
                 while stack:
                     node = stack.pop()
-                    person.append(node)
+                    cur.append(node)
 
                     for neighbor in graph[node][1]:
                         if neighbor not in seen:
                             seen.add(neighbor)
                             stack.append(neighbor)
 
-                person.sort()
-                output.append([graph[mail][0]] + person)
+                cur.sort()
+                output.append([name] + cur)
         
         return output
