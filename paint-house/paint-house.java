c42deac040c4c509e8309f81ae05336a52032775
Jeremy Tsai <jetsai@hmc.edu>
2021-03-05 16:26:20 -0800

    Time: 0 ms (100.00%), Space: 38.3 MB (75.80%) - LeetHub

--- paint-house/paint-house.java
+++ paint-house/paint-house.java
@@ -4,26 +4,13 @@ class Solution {
             return 0;
         }
         
-        int minimum = min(costs[costs.length - 1]);
         
         for (int i = costs.length - 2; i >= 0; i--) {
             costs[i][0] = costs[i][0] + Math.min(costs[i+1][1], costs[i+1][2]);
             costs[i][1] = costs[i][1] + Math.min(costs[i+1][0], costs[i+1][2]);
             costs[i][2] = costs[i][2] + Math.min(costs[i+1][0], costs[i+1][1]);
-
-            minimum = Math.min(Math.min(costs[i][0], costs[i][1]), costs[i][2]);
         }
         
-        return minimum;
-    }
-    
-    public int min(int[] arr) {
-        int output = Integer.MAX_VALUE;
-        
-        for (int i : arr) {
-            output = Math.min(output, i);
-        }
-        
-        return output;
+        return Math.min(Math.min(costs[0][0], costs[0][1]), costs[0][2]);
     }
 }
