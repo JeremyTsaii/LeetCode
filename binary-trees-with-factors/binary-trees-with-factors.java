1c42e54eec4e3f3c82796b4af7e469fa3d93bd81
Jeremy Tsai <jetsai@hmc.edu>
2021-03-14 11:34:33 -0700

    Time: 34 ms (37.17%), Space: 39.4 MB (42.48%) - LeetHub

--- binary-trees-with-factors/binary-trees-with-factors.java
+++ binary-trees-with-factors/binary-trees-with-factors.java
@@ -1,24 +1,19 @@
 class Solution {
     public int numFactoredBinaryTrees(int[] arr) {
         Arrays.sort(arr);
-        Map<Integer,Long> map = new HashMap<>();
-        map.put(arr[0], 1l);
+        long count = 0l, mod = (long)1e9 + 7;
+        Map<Integer,Long> dp = new HashMap<>();
         
-        for (int i = 1; i < arr.length; i++) {
-            long cur = 1;
-            for (int n : map.keySet()) {
-                if(arr[i] % n == 0 && map.containsKey(arr[i] / n)) {
-                    cur += map.get(arr[i] / n) * map.get(n);
+        for (int i = 0; i < arr.length; i++) {
+            dp.put(arr[i], 1l);
+            for (int j = 0; j < i; j++) {
+                if(arr[i] % arr[j] == 0) {
+                    dp.put(arr[i], (dp.get(arr[i]) + dp.get(arr[j]) * dp.getOrDefault(arr[i]/arr[j], 0l)) % mod);
                 }
             }
-            map.put(arr[i], cur);
+            count = (count + dp.get(arr[i])) % mod;
         }
         
-        long sum = 0;
-        for (long c : map.values()) {
-            sum = (sum + c) % 1000000007;
-        }
-        
-        return (int) sum;
+        return (int) count;
     }
 }
