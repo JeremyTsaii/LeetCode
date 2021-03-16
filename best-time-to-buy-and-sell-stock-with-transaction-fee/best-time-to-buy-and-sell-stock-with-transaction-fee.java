8e24c63a38b97f42003fad652b34a8ae9c6c5805
Jeremy Tsai <jetsai@hmc.edu>
2021-03-16 12:23:02 -0700

    Time: 3 ms (93.90%), Space: 48.2 MB (76.19%) - LeetHub

--- best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -4,15 +4,14 @@ class Solution {
             return 0;
         }
         
-        int[] buy = new int[prices.length];
-        buy[0] = -prices[0];
-        int[] sell = new int[prices.length];
+        int buy = -prices[0];
+        int sell = 0;
         
         for (int i = 1; i < prices.length; i++) {
-            buy[i] = Math.max(buy[i-1], sell[i-1] - prices[i]);
-            sell[i] = Math.max(sell[i-1], buy[i-1] + prices[i] - fee);
+            buy = Math.max(buy, sell - prices[i]);
+            sell = Math.max(sell, buy + prices[i] - fee);
         }
         
-        return sell[prices.length - 1];
+        return sell;
     }
 }
