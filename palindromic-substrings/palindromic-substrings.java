cdc864895cc3a025444775113e88a71a7ffddf31
Jeremy Tsai <jetsai@hmc.edu>
2021-03-27 11:24:15 -0700

    Time: 2 ms (97.17%), Space: 36.9 MB (91.92%) - LeetHub

---
+++ palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            count += expand(i, i, s);
+            count += expand(i, i+1, s);
+        }
+        
+        return count;
+    }
+    
+    public int expand(int start, int end, String s) {
+        if (end >= s.length()) {
+            return 0;
+        }
+        
+        int count = 0;
+        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+            count++;
+            start--;
+            end++;
+        }
+        
+        return count;
+    }
+}
