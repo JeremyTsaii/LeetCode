c309aa9d16d794033603ec768f74a68f7f660e05
Jeremy Tsai <jetsai@hmc.edu>
2021-03-29 09:58:45 -0700

    Time: 4 ms (100.00%), Space: 44.7 MB (46.14%) - LeetHub

---
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0, j = matrix[0].length - 1;
+        
+        while(i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (target < matrix[i][j]) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        
+        
+        return false;
+    }
+}
