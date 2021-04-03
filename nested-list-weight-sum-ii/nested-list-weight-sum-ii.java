aafad18365bddff52391eba718a2d8bcea7cc3f1
Jeremy Tsai <jetsai@hmc.edu>
2021-04-02 22:30:21 -0700

    Time: 0 ms (100.00%), Space: 36.8 MB (20.62%) - LeetHub

---
+++ nested-list-weight-sum-ii/nested-list-weight-sum-ii.java
@@ -0,0 +1,59 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+class Solution {
+    
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        return recurse(nestedList, maxDepth(nestedList), 1);
+    }
+    
+    public int recurse(List<NestedInteger> nestedList, int maxDepth, int depth) {
+        int sum = 0;
+        
+        for (NestedInteger n : nestedList) {
+            if (n.isInteger()) {
+                sum += n.getInteger() * (maxDepth - depth + 1);
+            } else {
+                sum += recurse(n.getList(), maxDepth, depth + 1);
+            }
+        }
+        
+        return sum;
+    }
+    
+    public int maxDepth(List<NestedInteger> nestedList) {
+        int max = 1;
+        for (NestedInteger n : nestedList) {
+            if (!n.isInteger()) {
+                max = Math.max(max, 1 + maxDepth(n.getList()));
+            }
+        }
+        
+        return max;
+    }
+}
