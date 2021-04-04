2dd45299bb6b7f54a21672e3685941acc2ec31bd
Jeremy Tsai <jetsai@hmc.edu>
2021-04-04 10:41:17 -0700

    Time: 2 ms (68.47%), Space: 38.8 MB (85.07%) - LeetHub

---
+++ longest-valid-parentheses/longest-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int max = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    max = Math.max(max, i - stack.peek());
+                }
+            }
+        }
+        
+        return max;
+    }
+}
