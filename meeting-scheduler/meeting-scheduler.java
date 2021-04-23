0eb85f45375dc5e3825ddd9e83630b1277b0f61e
Jeremy Tsai <jetsai@hmc.edu>
2021-04-23 14:45:10 -0700

    Time: 25 ms (67.14%), Space: 48.5 MB (30.54%) - LeetHub

---
+++ meeting-scheduler/meeting-scheduler.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
+        Arrays.sort(slots1, (a, b) -> (a[0] - b[0]));
+        Arrays.sort(slots2, (a, b) -> (a[0] - b[0]));
+        
+        // System.out.println(Arrays.deepToString(slots1));
+        // System.out.println(Arrays.deepToString(slots2));
+
+        
+        List<Integer> output = new ArrayList<>();
+        
+        int i1 = 0, i2 = 0;
+        
+        while(i1 < slots1.length && i2 < slots2.length) {
+            int[] s1 = slots1[i1];
+            int[] s2 = slots2[i2];
+            
+            int start = Math.max(s1[0], s2[0]);
+            int end = Math.min(s1[1], s2[1]);
+            int overlap = end - start;
+            
+            if (overlap >= duration) {
+                output.add(start);
+                output.add(start + duration);
+                break;
+            } else if (s1[1] > s2[1]) {
+                i2++;
+            } else {
+                i1++;
+            }
+        }
+        
+        return output;
+    }
+}
