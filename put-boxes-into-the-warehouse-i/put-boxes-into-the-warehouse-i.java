b768377765f57a46d5f83176059a0a3e20611f2f
Jeremy Tsai <jetsai@hmc.edu>
2021-05-08 22:29:52 -0700

    Time: 14 ms (100.00%), Space: 51.6 MB (69.93%) - LeetHub

--- put-boxes-into-the-warehouse-i/put-boxes-into-the-warehouse-i.java
+++ put-boxes-into-the-warehouse-i/put-boxes-into-the-warehouse-i.java
@@ -1,25 +1,21 @@
 class Solution {
     public int maxBoxesInWarehouse(int[] boxes, int[] warehouse) {
-        int[] min = new int[warehouse.length];
-        min[0] = warehouse[0];
-        
-        for (int i = 1; i < warehouse.length; i++) {
-            min[i] = Math.min(warehouse[i], min[i-1]);
-        }
+        int idx = boxes.length - 1;
+        int count = 0;
         
         Arrays.sort(boxes);
         
-        int idx = 0;
-        int count = 0;
-        
-        for (int i = min.length - 1; i >= 0; i--) {
-            if (idx >= boxes.length) {
-                break;
+        for (int w : warehouse) {
+            while(idx >= 0 && boxes[idx] > w) {
+                idx--;
             }
-            if (min[i] >= boxes[idx]) {
-                count++;
-                idx++;
+            
+            if (idx == -1) {
+                return count;
             }
+            
+            count++;
+            idx--;
         }
         
         
