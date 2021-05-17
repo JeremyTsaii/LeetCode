8dc1fa8c9db33acc95367b459af1aa843145aa68
Jeremy Tsai <jetsai@hmc.edu>
2021-05-17 14:40:17 -0700

    Time: 10 ms (34.84%), Space: 39.8 MB (58.05%) - LeetHub

--- shortest-bridge/shortest-bridge.java
+++ shortest-bridge/shortest-bridge.java
@@ -3,44 +3,33 @@ class Solution {
         
         Queue<Pair<Pair<Integer,Integer>,Integer>> q = new LinkedList<>();
         int[][] dirs = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
-        boolean found = false;
-        
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
-                    found = true;
-                    Stack<Pair<Integer,Integer>> stack = new Stack<>();
-                    stack.push(new Pair<>(i, j));
-                    grid[i][j] = 2;
-                    q.offer(new Pair<>(new Pair<>(i, j), 0));
-                    
-                    while(!stack.isEmpty()) {
-                        Pair<Integer,Integer> pair = stack.pop();
-                        int ii = pair.getKey();
-                        int jj = pair.getValue();
-                        
-                        for (int[] dir : dirs) {
-                            int di = dir[0];
-                            int dj = dir[1];
-                            
-                            int i2 = ii + di;
-                            int j2 = jj + dj;
-                            
-                            if (i2 >= 0 && i2 < grid.length && j2 >= 0 && j2 < grid[0].length && grid[i2][j2] == 1) {
-                                stack.push(new Pair<>(i2, j2));
-                                grid[i2][j2] = 2;
-                                q.offer(new Pair<>(new Pair<>(i2, j2), 0));
 
-                            }
-                        }
-                    }
+        Stack<Pair<Integer,Integer>> stack = new Stack<>();
+        Pair<Integer,Integer> firstPair = firstCoord(grid);
+        int i = firstPair.getKey();
+        int j = firstPair.getValue();
+        stack.push(new Pair<>(i, j));
+        grid[i][j] = 2;
+        q.offer(new Pair<>(new Pair<>(i, j), 0));
+
+        while(!stack.isEmpty()) {
+            Pair<Integer,Integer> pair = stack.pop();
+            int ii = pair.getKey();
+            int jj = pair.getValue();
+
+            for (int[] dir : dirs) {
+                int di = dir[0];
+                int dj = dir[1];
+
+                int i2 = ii + di;
+                int j2 = jj + dj;
+
+                if (i2 >= 0 && i2 < grid.length && j2 >= 0 && j2 < grid[0].length && grid[i2][j2] == 1) {
+                    stack.push(new Pair<>(i2, j2));
+                    grid[i2][j2] = 2;
+                    q.offer(new Pair<>(new Pair<>(i2, j2), 0));
+
                 }
-                if (found) {
-                    break;
-                }
-            }
-            if (found) {
-                break;
             }
         }
         
@@ -72,5 +61,16 @@ class Solution {
             }
         }
         return 0;
+    }
+    
+    public Pair<Integer,Integer> firstCoord(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    return new Pair<>(i, j);
+                }
+            }
+        }
+        return new Pair<>(0,0);
     }
 }
