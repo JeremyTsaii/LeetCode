3b181a64c91796d978a96a17201bf06038709bde
Jeremy Tsai <jetsai@hmc.edu>
2021-05-17 14:35:37 -0700

    Time: 9 ms (38.45%), Space: 40.2 MB (13.38%) - LeetHub

---
+++ shortest-bridge/shortest-bridge.java
@@ -0,0 +1,76 @@
+class Solution {
+    public int shortestBridge(int[][] grid) {
+        
+        Queue<Pair<Pair<Integer,Integer>,Integer>> q = new LinkedList<>();
+        int[][] dirs = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+        boolean found = false;
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    found = true;
+                    Stack<Pair<Integer,Integer>> stack = new Stack<>();
+                    stack.push(new Pair<>(i, j));
+                    grid[i][j] = 2;
+                    q.offer(new Pair<>(new Pair<>(i, j), 0));
+                    
+                    while(!stack.isEmpty()) {
+                        Pair<Integer,Integer> pair = stack.pop();
+                        int ii = pair.getKey();
+                        int jj = pair.getValue();
+                        
+                        for (int[] dir : dirs) {
+                            int di = dir[0];
+                            int dj = dir[1];
+                            
+                            int i2 = ii + di;
+                            int j2 = jj + dj;
+                            
+                            if (i2 >= 0 && i2 < grid.length && j2 >= 0 && j2 < grid[0].length && grid[i2][j2] == 1) {
+                                stack.push(new Pair<>(i2, j2));
+                                grid[i2][j2] = 2;
+                                q.offer(new Pair<>(new Pair<>(i2, j2), 0));
+
+                            }
+                        }
+                    }
+                }
+                if (found) {
+                    break;
+                }
+            }
+            if (found) {
+                break;
+            }
+        }
+        
+        
+        while(!q.isEmpty()) {
+            Pair<Pair<Integer,Integer>,Integer> pair = q.poll();
+            Pair<Integer,Integer> pair2 = pair.getKey();
+            int steps = pair.getValue();
+            int ii = pair2.getKey();
+            int jj = pair2.getValue();
+            
+            
+            for (int[] dir : dirs) {
+                int di = dir[0];
+                int dj = dir[1];
+                
+                int i2 = di + ii;
+                int j2 = dj + jj;
+                
+                if (i2 >= 0 && i2 < grid.length && j2 >= 0 && j2 < grid[0].length) {
+                    if (grid[i2][j2] == 0) {
+                        q.offer(new Pair<>(new Pair<>(i2, j2), steps + 1));
+                        grid[i2][j2] = 2;
+                    } else if (grid[i2][j2] == 1) {
+                        return steps;
+                    }
+                    
+                }
+            }
+        }
+        return 0;
+    }
+}
