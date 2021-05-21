525849eb9b5f4f99c2f2416122100e687ec9e1fa
Jeremy Tsai <jetsai@hmc.edu>
2021-05-21 02:01:58 -0700

    Time: 21 ms (8.26%), Space: 41.8 MB (5.38%) - LeetHub

--- isomorphic-strings/isomorphic-strings.java
+++ isomorphic-strings/isomorphic-strings.java
@@ -1,35 +1,19 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        Map<Character,List<Integer>> map = new HashMap<>();
-        Map<Character,List<Integer>> map2 = new HashMap<>();
+        Map<Character,Integer> map = new HashMap<>();
+        Map<Character,Integer> map2 = new HashMap<>();
 
-        
+
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            List<Integer> existing = map.getOrDefault(c, new ArrayList<>());
-            existing.add(i);
-            map.put(c, existing);
+            int idx1 = map.getOrDefault(s.charAt(i), -1);
+            int idx2 = map2.getOrDefault(t.charAt(i), -1);
             
-            char c2 = t.charAt(i);
-            List<Integer> existing2 = map2.getOrDefault(c2, new ArrayList<>());
-            existing2.add(i);
-            map2.put(c2, existing2);
-        }
-        
-        return check(map, t) && check(map2, s);        
-
-    }
-    
-    public boolean check(Map<Character,List<Integer>> map, String s) {
-        for (List<Integer> arr : map.values()) {
-            if (arr.size() > 1) {
-                char anchor = s.charAt(arr.get(0));
-                for (int i = 1; i < arr.size(); i++) {
-                    if (s.charAt(arr.get(i)) != anchor) {
-                        return false;
-                    }
-                }
+            if (idx1 != idx2) {
+                return false;
             }
+            
+            map.put(s.charAt(i), i);
+            map2.put(t.charAt(i), i);
         }
         
         return true;
